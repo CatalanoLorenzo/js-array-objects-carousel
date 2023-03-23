882c95f3955b38c66fb804dff3bd6dcb99ddb746
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-23 19:36:08 +0100

    add bonus 2

--- asset/js/main.js
+++ asset/js/main.js
@@ -41,37 +41,61 @@ const colEl = document.querySelector('.col-6')
 const showColEl = document.querySelector('.col-3')
 const backButton = document.querySelector('.back_button')
 const nextButton = document.querySelector('.next_button')
+const stopButton = document.querySelector('.stop_button')
+const playButton = document.querySelector('.play_button')
 let imgvisible = 0
-
-images.forEach((array,index) => {
+let numberorder
+const timingInterval = 1000
+const timingTimeout = 3000
+images.forEach((array, index) => {
     const src = array.image
     const title = array.title
     const text = array.text
-    const numberorder = index
+    const order = index
+    numberorder = order
     console.log(src)
-    Generat_input_card(src,title,text,imgvisible,numberorder)
-    generet_list_img(src,imgvisible,numberorder)
+    Generat_input_card(src, title, text, imgvisible, order)
+    generet_list_img(src, imgvisible, order)
 })
 
 const cardsLoad = document.querySelectorAll('.col-6 .card');
 const allCardsLoad = document.querySelectorAll('.col-3 .card');
+
+setTimeout(() => {
+    const idInterval = setInterval(() => {
+        imgvisible = next(cardsLoad, imgvisible, allCardsLoad, images)
+        console.log('fine intervallo');
+    }, (timingInterval))
+    stopButton.addEventListener('click', function () {
+        clearInterval(idInterval)
+        console.log('stop intervallo');
+    })
+    playButton.addEventListener('click', function () {
+        idInterval = setInterval(() => {
+            imgvisible = next(cardsLoad, imgvisible, allCardsLoad, images)
+            console.log('fine intervallo');
+        }, (timingInterval))
+        console.log('play intervallo');
+    })
+}, (timingTimeout))
+
 nextButton.addEventListener('click', function () {
-    const imgCurrentVisible = cardsLoad[imgvisible]
-    const imgAllCurrentVisible = allCardsLoad[imgvisible]
-
-    console.log(imgCurrentVisible);
-
-    imgCurrentVisible.classList.remove('displyon')
-    imgAllCurrentVisible.classList.remove('primary')
-    if (imgvisible == images.length - 1) {
-        imgvisible = 0;
-    }else{
-        imgvisible++;
-    }
-    const nextimgCurrentVisible = cardsLoad[imgvisible]
-    const nextAllImgCurrentVisible = allCardsLoad[imgvisible]
-    nextimgCurrentVisible.classList.add('displyon')
-    nextAllImgCurrentVisible.classList.add('primary')
+    imgvisible = next(cardsLoad, imgvisible, allCardsLoad, images)
+    /*  const imgCurrentVisible = cardsLoad[imgvisible]
+     const imgAllCurrentVisible = allCardsLoad[imgvisible]
+ 
+ 
+     imgCurrentVisible.classList.remove('displyon')
+     imgAllCurrentVisible.classList.remove('primary')
+     if (imgvisible == images.length - 1) {
+         imgvisible = 0;
+     }else{
+         imgvisible++;
+     }
+     const nextimgCurrentVisible = cardsLoad[imgvisible]
+     const nextAllImgCurrentVisible = allCardsLoad[imgvisible]
+     nextimgCurrentVisible.classList.add('displyon')
+     nextAllImgCurrentVisible.classList.add('primary') */
 });
 
 backButton.addEventListener('click', function () {
@@ -85,9 +109,10 @@ backButton.addEventListener('click', function () {
 
     if (imgvisible == 0) {
         imgvisible = images.length - 1;
-    }else{
+    } else {
         imgvisible--;
     }
+    console.log(imgvisible);
     const nextAllImgEl = cardsLoad[imgvisible]
     const nextAllImgCurrentVisible = allCardsLoad[imgvisible]
 
@@ -101,50 +126,84 @@ backButton.addEventListener('click', function () {
 
 
 
-function generet_list_img(srcimg,imgvisible,numberorder) {
+function generet_list_img(srcimg, imgvisible, numberorder) {
     const cards = document.createElement('div')
-    
+
     if (numberorder == imgvisible) {
-        cards.classList.add('card','displyon','primary')
-    }else{
-        cards.classList.add('card','displyon')
+        cards.classList.add('card', 'displyon', 'primary')
+    } else {
+        cards.classList.add('card', 'displyon')
     }
-    showColEl.insertAdjacentElement('beforeend',cards)
-    const imgEl = document.createElement('img') 
-    cards.insertAdjacentElement('beforeend',imgEl)
+    showColEl.insertAdjacentElement('beforeend', cards)
+    const imgEl = document.createElement('img')
+    cards.insertAdjacentElement('beforeend', imgEl)
     imgEl.src = `./asset/${srcimg}`
 }
 
 
 
-function Generat_input_card(srcimg,title,text,imgvisible,numberorder) {
+function Generat_input_card(srcimg, title, text, imgvisible, numberorder) {
 
     const cards = document.createElement('div')
-    
+
     if (numberorder == imgvisible) {
-        cards.classList.add('card','displyon')
-    }else{
+        cards.classList.add('card', 'displyon')
+    } else {
         cards.classList.add('card',)
     }
-    colEl.insertAdjacentElement('beforeend',cards)
-    
+    colEl.insertAdjacentElement('beforeend', cards)
+
     const cardHeader = document.createElement('div')
     cardHeader.classList.add('card-header')
-    cards.insertAdjacentElement('beforeend',cardHeader)
+    cards.insertAdjacentElement('beforeend', cardHeader)
 
     const h1El = document.createElement('h1')
-    cardHeader.insertAdjacentElement('beforeend',h1El)
+    cardHeader.insertAdjacentElement('beforeend', h1El)
     h1El.innerText = title
 
     const cardBody = document.createElement('div')
     cardBody.classList.add('card-body')
-    cards.insertAdjacentElement('beforeend',cardBody)
+    cards.insertAdjacentElement('beforeend', cardBody)
 
     const imgEl = document.createElement('img')
-    cardBody.insertAdjacentElement('beforeend',imgEl)
+    cardBody.insertAdjacentElement('beforeend', imgEl)
     imgEl.src = `./asset/${srcimg}`
-   
+
     const pEl = document.createElement('p')
-    cardBody.insertAdjacentElement('beforeend',pEl)
+    cardBody.insertAdjacentElement('beforeend', pEl)
     pEl.innerText = text
+}
+/**
+ * 
+ * @param {Element} cardsLoad 
+ * @param {Number} imgvisible 
+ * @param {Element} allCardsLoad 
+ * @param {Array} images 
+ */
+function next(cardsLoad, imgvisible, allCardsLoad, images) {
+
+    remove_class(cardsLoad, imgvisible, allCardsLoad)
+    console.log(imgvisible);
+    console.log(images.length);
+
+    if (imgvisible == images.length - 1) {
+        imgvisible = 0;
+    } 
+    imgvisible++;
+    console.log(imgvisible);
+
+    add_class(cardsLoad, imgvisible, allCardsLoad)
+    return imgvisible
+}
+function remove_class(cardsLoad, imgvisible, allCardsLoad) {
+    const imgCurrentVisible = cardsLoad[imgvisible]
+    const imgAllCurrentVisible = allCardsLoad[imgvisible]
+    imgCurrentVisible.classList.remove('displyon')
+    imgAllCurrentVisible.classList.remove('primary')
+}
+function add_class(cardsLoad, imgvisible, allCardsLoad) {
+    const nextimgCurrentVisible = cardsLoad[imgvisible]
+    const nextAllImgCurrentVisible = allCardsLoad[imgvisible]
+    nextimgCurrentVisible.classList.add('displyon')
+    nextAllImgCurrentVisible.classList.add('primary')
 }
