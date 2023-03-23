5f0cbac795129969883a8fb595fcc519a7b4f9b9
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-23 16:26:36 +0100

    add function back and next

--- asset/js/main.js
+++ asset/js/main.js
@@ -37,19 +37,24 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
 //ciclo all'interno dell'array per ogni elemento
-images.forEach((img) => {
+images.forEach((img, index) => {
     //creo una constante e l'associo al valore della kay image
     const percorsoImg = img.image
     //creo una constante e l'associo al valore della kay title
     const titolo = img.title
     //creo una constante e l'associo al valore della kay text
     const testo = img.text
+
+    const j = index
+    console.log(j);
+
     //utilizzo le variabili generate in precedenza per usare la funzione
-    generate_row_col_img_src_and_h1_and_p(percorsoImg, titolo, testo)
+    generate_row_col_img_src_and_h1_and_p(percorsoImg, titolo, testo, images, j)
 
 })
-
 
 
 
@@ -63,31 +68,95 @@ images.forEach((img) => {
         <img src="" alt="">
     </div>
  */
-function generate_row_col_img_src_and_h1_and_p(percorsoImg, titolo, testo) {
+function generate_row_col_img_src_and_h1_and_p(percorsoImg, titolo, testo, array, index) {
+
+
+    let imgvisible = 0;
     //associa rowEl all'elemento del document con classe row
     const rowEl = document.querySelector('.row')
     //genera un div
     const colEl = document.createElement('div')
-    //aggiunge una classe al tag 'div'
-    colEl.classList.add('col')
+    //genera delle classe al col 'primary','col','principal_card' o 'secondary','col','principal_card'
+    if (index == imgvisible) {
+        colEl.classList.add('primary', 'col-3', 'principal_card', 'd-flex', 'align-items-center', 'flex-column')
+    } else {
+        colEl.classList.add('secondary', 'col-3', 'principal_card', 'd-none', 'align-items-center', 'flex-column')
+    }
     //genera un tag img 
     const imgEl = document.createElement('img')
+    //genera un src per img 
+    imgEl.src = `./asset/${percorsoImg}`
     //genera un tag h1
-    const h1El = document.createElement('h1')
-    //inserisce del testo rima della chiusura del h1
-    h1El.insertAdjacentText('beforeend', titolo)
+    const h4El = document.createElement('h4')
+    //inserisce del testo rima della chiusura del h4
+    h4El.insertAdjacentText('beforeend', titolo)
     //genera un tag p
     const pEl = document.createElement('p')
     //inserisce del testo rima della chiusura del p
     pEl.insertAdjacentText('beforeend', testo)
-    //genera un src per img 
-    imgEl.src = `./asset/${percorsoImg}`
     //inserisce prima della chiusura del 'div' con classe row il 'div' col
     rowEl.insertAdjacentElement('beforeend', colEl)
     //inserisce prima della chiusura del 'div' con classe col il tag img
     colEl.insertAdjacentElement('beforeend', imgEl)
-    //inserisce prima della chiusura del 'div' con classe col il tag h1
-    colEl.insertAdjacentElement('beforeend', h1El)
+    //inserisce prima della chiusura del 'div' con classe col il tag h4
+    colEl.insertAdjacentElement('beforeend', h4El)
     //inserisce prima della chiusura del 'div' con classe col il tag p
     colEl.insertAdjacentElement('beforeend', pEl)
+    const principlaCard = document.querySelector('.principal_card')
+    const conatinerButton = document.createElement('div')
+    conatinerButton.classList.add('container_button', 'w-100', 'justify-content-between', 'd-flex')
+    principlaCard.insertAdjacentElement('beforeend', conatinerButton)
+    const buttonB = document.createElement('button')
+    buttonB.classList.add('back_button')
+    const buttonN = document.createElement('button')
+    buttonN.classList.add('next_button')
+    console.log(buttonB,buttonN)
+    const buttonNext = document.querySelector('.next_button')
+    const buttonBack = document.querySelector('.back_button')
+    conatinerButton.insertAdjacentElement('beforeend', buttonB)
+    conatinerButton.insertAdjacentElement('beforeend', buttonN)
+
+
+    const cardLoad = document.querySelector('.principal_card', 'primary');
+    const allcardLoad = document.querySelector('.principal_card', 'primary');
+    buttonNext.addEventListener('click', function () {
+        const imgCurrentVisible = cardLoad[imgvisible]
+        const allImgCurrentVisible = allcardLoad[imgvisible]
+
+        allImgCurrentVisible.classList.remove('primary');
+        imgCurrentVisible.classList.remove('visibl');
+        imgvisible++;
+        if (imgvisible == array.length) {
+            imgvisible = 0;
+        }
+        selectImg(imgCurrentVisible, allImgCurrentVisible);
+        console.log(allcardLoad);
+    });
+
+    buttonBack.addEventListener('click', function () {
+        const imgCurrentVisible = cardLoad[imgvisible]
+        const allImgCurrentVisible = allcardLoad[imgvisible]
+        allImgCurrentVisible.classList.remove('primary');
+        imgCurrentVisible.classList.remove('visibl');
+        imgvisible--;
+        if (imgvisible < 0) {
+            imgvisible = (array.length - 1);
+        };
+        selectImg(imgCurrentVisible, allImgCurrentVisible);
+    });
+
+
+
+
+
+
+
+
+}
+function selectImg(imgCurrentVisible, allImgCurrentVisible) {
+
+    const nextAllImgEl = allcardLoad[imgvisible];
+    const nextImgEl = cardLoad[imgvisible];
+    nextAllImgEl.classList.add('primary');
+    nextImgEl.classList.add('visibl');
 }
